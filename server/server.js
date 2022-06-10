d7492a478a8263e84e99bd6a7458f6dd31aea3c7
Srinivasan K <80943324+srini165712@users.noreply.github.com>
2022-06-10 13:35:23 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const app = express();
+const passportSetup = require("./passport");
+const authRoute = require("./routes/auth");
+
+
+app.use(
+    cookieSession({
+        name: "session",
+        keys: ["toybank"],
+        maxAge: 24*60*60*100,
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: "GET,POST,PUT,DELETE",
+        credentials: true,
+    })
+);
+
+app.use("/auth", authRoute);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}`));
